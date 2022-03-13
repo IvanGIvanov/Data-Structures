4070718a7350e1c6d30765376aa17e17ee52b156
Ivan <ivanov.g.ivan@outlook.com>
2022-03-13 23:40:47 +0200

    Complete problem 3

--- Heaps-BST/03.MaxHeap/MaxHeap.cs
+++ Heaps-BST/03.MaxHeap/MaxHeap.cs
@@ -5,21 +5,95 @@
 
     public class MaxHeap<T> : IAbstractHeap<T> where T : IComparable<T>
     {
-        public int Size => throw new NotImplementedException();
+        private List<T> elements;
+
+        public MaxHeap()
+        {
+            this.elements = new List<T>();
+        }
+
+        public int Size => this.elements.Count;
 
         public void Add(T element)
         {
-            throw new NotImplementedException();
+            this.elements.Add(element);
+            this.HeapUp(this.elements.Count - 1);
+        }
+
+        private void HeapUp(int index)
+        {
+            var parentIndex = (index - 1) / 2;
+            while (index > 0 && this.elements[index].CompareTo(this.elements[parentIndex]) > 0)
+            {
+                this.Swap(index, parentIndex);
+                index = parentIndex;
+                parentIndex = (index - 1) / 2;
+            }
+        }
+
+        private void Swap(int index, int parentIndex)
+        {
+            var temp = this.elements[index];
+            this.elements[index] = this.elements[parentIndex];
+            this.elements[parentIndex] = temp;
+
         }
 
         public T ExtractMax()
         {
-            throw new NotImplementedException();
+            if (elements.Count == 0)
+            {
+                throw new InvalidOperationException();
+            }
+
+            T element = this.elements[0];
+            this.Swap(0, this.elements.Count - 1);
+            this.elements.RemoveAt(elements.Count - 1);
+            this.HeapDown(0);
+
+            return element;
+        }
+
+        private void HeapDown(int index)
+        {
+            var biggerChildIndex = this.GetBiggerChildIndex(index);
+
+            while ((biggerChildIndex < this.elements.Count && biggerChildIndex >= 0) && this.elements[biggerChildIndex].CompareTo(this.elements[index]) > 0)
+            {
+                this.Swap(biggerChildIndex, index);
+
+                index = biggerChildIndex;
+                biggerChildIndex = this.GetBiggerChildIndex(index);
+            }
+        }
+
+        private int GetBiggerChildIndex(int index)
+        {
+            var firstChildIndex = index * 2 + 1;
+            var secondChildIndex = index * 2 + 2;
+
+            if (secondChildIndex < this.elements.Count)
+            {
+                if (this.elements[firstChildIndex].CompareTo(this.elements[secondChildIndex]) > 0)
+                {
+                    return firstChildIndex;
+                }
+
+                return secondChildIndex;
+            }
+            else if (firstChildIndex < this.elements.Count)
+            {
+                return firstChildIndex;
+            }
+            else
+            {
+                return -1;
+            }
         }
 
         public T Peek()
         {
-            throw new NotImplementedException();
+            return this.elements[0];
         }
     }
 }
